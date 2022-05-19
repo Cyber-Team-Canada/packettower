bc5308ecd5dd479ef4b29142696cd4935e125f56
e-seng <eth.sengsavang@gmail.com>
2022-05-19 17:02:02 -0600

    Allow command line arguments to specify certain parameters
    this includes the location of the output file and the interface to
    listen to (which was previously committed)

--- packettower.py
+++ packettower.py
@@ -18,7 +18,6 @@ import subprocess as subp
 import sys
 import traceback
 
-pcap_file_base = "./test_dumps/dump"
 # randomly generate a tmp file for this process to use
 TEMPPATH = f"/tmp/{sha256(str(random.randint(0,10000000)).encode('utf-8')).hexdigest()}.pcap"
 
@@ -33,8 +32,9 @@ def listen(interface, pcap_file_base=None):
 
     capture = pyshark.LiveCapture(interface=interface, use_json=True, include_raw=True)
 
-    # start tcp_dump process
-    tcpdump_p = subp.Popen(["tcpdump", "-i", interface, "-w", TEMPPATH, "-U"])
+    # start tcp_dump process if output path is specified
+    if(pcap_file_base != None):
+        tcpdump_p = subp.Popen(["tcpdump", "-i", interface, "-w", TEMPPATH, "-U"])
 
     while(True):
         for packet in capture.sniff_continuously(packet_count=25):
@@ -76,51 +76,46 @@ def listen(interface, pcap_file_base=None):
                 print("-----------------------")
                 continue
 
-        # close and restart tcpdump process to write pcap file
-        tcpdump_p.terminate()
-        # move generated pcap file to desired location
-        shutil.copy(TEMPPATH, pcap_file_base+"-"+datetime.now().strftime("%H-%M-%S-%sss")+".pcap")
-        tcpdump_p = subp.Popen(["tcpdump", "-i", interface, "-w", TEMPPATH, "-U"])
+        if(pcap_file_base != None):
+            # close and restart tcpdump process to write pcap file
+            tcpdump_p.terminate()
+            # move generated pcap file to desired location
+            shutil.copy(TEMPPATH, pcap_file_base+"/packettower_dump-"+datetime.now().strftime("%H-%M-%S-%s")+".pcap")
+            tcpdump_p = subp.Popen(["tcpdump", "-i", interface, "-w", TEMPPATH, "-U"])
+
+def print_help():
+    print(f"Usage: {sys.argv[0]} <interface> [options]")
+    print("optional args:")
+    print("| -o /path/to/dump/folder - write to the location to write files\n" \
+          "|    note: this will generate a file called packettower_dump-%H-%M-%S-%s.pcap" \
+          "if not specified, no pcap file will be generated.\n" \
+          "| -h - displays this help menu")
 
 if __name__ == "__main__":
     if(len(sys.argv) < 2):
-        print(f"Usage: {sys.argv[0]} <interface>")
+        print_help()
         exit(1)
 
-    # todo: ensure written pcap file is readable by tcpdump
-    # set headers of pcap file
-    # with open(DUMPPATH, "wb+") as pcap_file:
-    #     pcap_file.write(
-    #             b"\xd4\xc3"\
-    #             b"\xb2\xa1"\
-    #             b"\x02\x00"\
-    #             b"\x04\x00"\
-    #             b"\x00\x00"\
-    #             b"\x00\x00"\
-    #             b"\x00\x00"\
-    #             b"\x00\x00"\
-    #             b"\xff\xff"\
-    #             b"\xff\xff"\
-    #             b"\x01\x00"\
-    #             b"\x00\x00"\
-    #             b"\xb5\xd5"\
-    #             b"\x85\x62"\
-    #             b"\x92\xac"\
-    #             b"\x01\x00"\
-    #             b"\x4a\x00"\
-    #             b"\x00\x00"\
-    #             b"\x4a\x00"\
-    #             b"\x00\x00"
-    # )
+    # parse arguments
+    interface = None
+    pcap_file_base = None
 
-    listen(sys.argv[1])
-
+    for index, arg in enumerate(sys.argv):
+        if(arg[0] != '-' and interface == None):
+            interface = arg
+        if(arg == "-o"):
+            pcap_file_base = sys.argv[index+1]
+            continue
+        if(arg == "-h"):
+            print_help()
+            exit(0)
 
     try:
-        listen(sys.argv[1])
+        listen(sys.argv[1], pcap_file_base)
     except Exception as e:
         print(f"[err] General execption thrown:")
         traceback.print_exc()
         # cleanup
-        tcpdump_p.terminate()
-        os.remove(TEMPPATH)
+        if(pcap_file_base != None):
+            tcpdump_p.terminate()
+            os.remove(TEMPPATH)
