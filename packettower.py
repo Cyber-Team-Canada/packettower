b31d0de9534cc7a4ee8f4524af07a8b05c54e254
e-seng <eth.sengsavang@gmail.com>
2022-05-18 21:17:48 -0600

    Allow for interface selection when starting script

--- packettower.py
+++ packettower.py
@@ -2,7 +2,29 @@
 
 import pyshark
 
-capture = pyshark.LiveCapture(interface="wlan0")
+def listen(interface):
+    print(f"[info] capturing on interface {interface}")
+    print("[note] to exit, send two SIGINTs")
 
-for packet in capture.sniff_continuously(packet_count=5):
-    print(packet)
+    capture = pyshark.LiveCapture(interface=interface)
+
+    while(True):
+        for packet in capture.sniff_continuously(packet_count=10):
+            try:
+                print(packet.tcp.payload)
+            except AttributeError:
+                print("[info] packet has no data")
+                continue
+            except Exception as e:
+                print(f"[err] General execption thrown:")
+                print(e)
+
+if __name__ == "__main__":
+    import sys
+
+    if(len(sys.argv) < 2):
+        print(f"Usage: {sys.argv[0]} <interface>")
+        exit(1)
+
+    listen(sys.argv[1])
+
