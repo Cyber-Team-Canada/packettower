dd8a05d058627d705972d7f6ad9eee13a360aa18
e-seng <eth.sengsavang@gmail.com>
2022-05-31 15:31:46 -0600

    Start tcpdump processes per attacker per service

--- packettower.py
+++ packettower.py
@@ -26,6 +26,7 @@ tcpdump_p = None
 nullfd = open(os.devnull, "w")
 
 def listen(interface, service_port, pcap_file_base=None):
+    port_listen = False
     global tcpdump_p
     # mapping: port number : (filename, tcpdump_process)
     port_pcap_map = {}
@@ -70,6 +71,27 @@ def listen(interface, service_port, pcap_file_base=None):
                     dst_port = packet.tcp.port[1]
             except AttributeError:
                 continue
+
+            # if it's a new port making a request, start listening to it with a
+            # new tcpdump process
+
+            # one port must be the service port, ignore it
+            attacker_port = src_port
+            attacker_addr = src_addr
+            if(src_port == service_port):
+                attacker_port = dst_port
+                attacker_addr = dst_addr
+
+            if(attacker_port not in port_pcap_map.keys()):
+                pcap_path = f"{pcap_file_base}/packettower_port-{service_port}_{attacker_addr}"
+                if(port_listen): pcap_path += f"-{attacker_port}"
+
+                # filter for only this service
+                tcpdump_expr = f"port {service_port} and host {attacker_addr}"
+                if(port_listen) tcpdump_expr += f" and port {attacker_port}"
+
+                tcpdump_proc = subp.Popen(["tcpdump", "-i", interface, "-w", pcap_path, "-U"] + tcpdump_expr.split())
+                port_pcap_map[attacker_port] = (pcap_path, tcpdump_proc)
             # attempt to decode payload if it exists
             try:
                 raw_payload = payload.replace(':', '')
@@ -93,7 +115,9 @@ def listen(interface, service_port, pcap_file_base=None):
                 stderr=nullfd)
 
 def print_help():
-    print(f"Usage: {sys.argv[0]} <interface> [options]")
+    print(f"Usage: {sys.argv[0]} <interface> <port> [options]")
+    print("Packettower will generate .pcap files for a given service hosted at" \
+          "a specific port")
     print("optional args:")
     print("| -o /path/to/dump/folder - write to the location to write files\n" \
           "|    note: this will generate a file called packettower_dump-%H-%M-%S-%s.pcap" \
@@ -101,17 +125,22 @@ def print_help():
           "| -h - displays this help menu")
 
 if __name__ == "__main__":
-    if(len(sys.argv) < 2):
+    if(len(sys.argv) < 3):
         print_help()
         exit(1)
 
     # parse arguments
     interface = None
+    port = None
     pcap_file_base = None
 
     for index, arg in enumerate(sys.argv):
         if(arg[0] != '-' and interface == None):
             interface = arg
+            continue
+        if(arg[0] != '-' and port == None):
+            port = arg
+            continue
         if(arg == "-o"):
             pcap_file_base = sys.argv[index+1]
             continue
@@ -120,7 +149,7 @@ if __name__ == "__main__":
             exit(0)
 
     try:
-        listen(sys.argv[1], pcap_file_base)
+        listen(sys.argv[1], port, pcap_file_base)
     except Exception as e:
         print(f"[err] General execption thrown:")
         traceback.print_exc()
